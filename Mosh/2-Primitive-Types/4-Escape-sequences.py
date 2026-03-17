cbdf60c7113f1926edb065377c956bb532603137
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2026-03-17 22:39:06 +0300

    I have added code to escape sequence

---
+++ Mosh/2-Primitive-Types/4-Escape-sequences.py
@@ -0,0 +1,11 @@
+course = "Python Programming"
+
+# What if we want to add double quoetes between 
+
+course = "Python \"Programming"
+
+# Also we can use single quotes and double quotes.
+
+course = 'Python "Programming"'
+
+print(course)
